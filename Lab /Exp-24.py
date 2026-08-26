916b6ff14df9797b88af3493a9189de67219dcf9
192472173 <192472173.simats@saveetha.com>
2026-08-26 08:56:28 +0530

    Add dialog act classification based on input

---
+++ Lab /Exp-24.py
@@ -0,0 +1,10 @@
+dialog = input("Enter sentence: ")
+
+if dialog.endswith("?"):
+    print("Dialog Act: Question")
+
+elif dialog.lower().startswith("please"):
+    print("Dialog Act: Request")
+
+else:
+    print("Dialog Act: Statement")
